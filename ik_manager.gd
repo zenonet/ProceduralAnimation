613ef014462612c595aa0852ed19b7762004bcfb
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-24 13:47:04 +0200

    Made the legs move the body and not the other way around, made legs lift their feet, allowed controlling body height and other animation imrpovements

--- ik_manager.gd
+++ ik_manager.gd
@@ -4,11 +4,13 @@ extends SkeletonIK3D
 @export var raycast: RayCast3D
 
 @onready var current_desired_target: Vector3 = Vector3.ZERO
+var step_start_pos:Vector3 = Vector3.ZERO
+var current_pos_on_ground:Vector3 = Vector3.ZERO
+
+
 @onready var robot = $"../../.."
 
 var leg_in_air:bool = false
-
-
 
 func _ready():
 	start()
@@ -17,27 +19,41 @@ func _process(delta):
 	var below = raycast.get_collision_point()
 	var resting_pos = robot.global_position
 	resting_pos.y = below.y
-	var distance:float = (resting_pos).distance_to(target_obj.position)
+	var distance:float = current_pos_on_ground.distance_to(below)
 	if !raycast.is_colliding():
 		pass
 		# print("Not colliding")
 	# print("Distance: %d, below.z: %d, target.z: %d, raycast.z: %d" % [distance, below.z, target_obj.global_position.z, raycast.global_position.z])
 	
 	
-	var max_distance = max(3.5 * robot.velocity, 0.5)
+	var max_distance = max(2 * robot.velocity, 0.5)
 	print("Max distance: %f; actual distance: %f" % [max_distance, distance])
-	if (distance > max_distance) && robot.legs_in_air < 2:
+	if (distance > max_distance) && (robot.legs_in_air < 2 || false):
 		current_desired_target = below
 		leg_in_air = true
 		robot.legs_in_air += 1
-		print("Stepping")
+		step_starting_point = current_pos_on_ground
+		step_length = distance
+		# print("Starting to step to %v" % current_desired_target)
 	elif distance > max_distance:
 		pass
 		# print("step suppressed by leg threshold")
 		
-
-	target_obj.global_position = target_obj.global_position.move_toward(current_desired_target, 40*delta)
+	if leg_in_air:
+		move_foot(delta)
+		
+var step_length:float = 0
+@onready var step_starting_point:Vector3 = target_obj.global_position 
+func move_foot(delta):
+	current_pos_on_ground = current_pos_on_ground.move_toward(current_desired_target, 38*delta)
 	
-	if leg_in_air && target_obj.global_position.distance_to(current_desired_target) < 0.05:
+	var progress = current_pos_on_ground.distance_to(current_desired_target) / step_length
+	#print(progress)
+	target_obj.global_position = current_pos_on_ground
+	
+	target_obj.global_position.y += sin(progress*PI)*1.2*robot.velocity
+	
+	if current_pos_on_ground.distance_to(current_desired_target) < 0.2:
 		robot.legs_in_air -= 1
 		leg_in_air = false
+	
--- robot.gd
+++ robot.gd
@@ -15,19 +15,33 @@ func _process(delta):
 	var movement = Vector2(
 		Input.get_axis("backward", "forward"),
 		Input.get_axis("right", "left"),
-	).normalized()
+	)
 	
-	velocity = movement.length()
-	print(velocity)
+	if(movement != Vector2.ZERO):
+		movement = movement.normalized()
 	
-	$Raycasts.global_position = self.global_position - (transform.basis.x*movement.y + transform.basis.z*movement.x) * 2.5
+	velocity = movement.length()+rot*delta
+	#print(velocity)
+	
+	var body_heigh_adjustment:float = Input.get_axis("body_down", "body_up")
+	body_height += body_heigh_adjustment * 0.05
+	
+	# Move leg targets
+	$Raycasts.global_position = self.global_position - (transform.basis.x*movement.y + transform.basis.z*movement.x) * 3
 	
 	# movement
-	global_position += -transform.basis.z * speed * delta * movement.x
-	global_position += -transform.basis.x * speed * delta * movement.y
+	# global_position += -transform.basis.z * speed * delta * movement.x
+	# global_position += -transform.basis.x * speed * delta * movement.y
 	
-	update_body_height()
+	# set body position to average of leg position
+	var avg_x = ($Armature/BL_Target.global_position.x + $Armature/BR_Target.global_position.x + $Armature/FL_Target.global_position.x + $Armature/FR_Target.global_position.x)/4
+	var avg_z = ($Armature/BL_Target.global_position.z + $Armature/BR_Target.global_position.z + $Armature/FL_Target.global_position.z + $Armature/FR_Target.global_position.z)/4
+	
+	global_position.x = move_toward(global_position.x, avg_x, 20*delta)
+	global_position.z = move_toward(global_position.z, avg_z, 20*delta)
+	
+	update_body_height(delta)
 
-func update_body_height():
+func update_body_height(delta):
 	var avg_ground_pos = ($Armature/BL_Target.global_position.y + $Armature/BR_Target.global_position.y + $Armature/FL_Target.global_position.y + $Armature/FR_Target.global_position.y)/4
-	global_position.y = move_toward(global_position.y, avg_ground_pos + body_height, 0.1)
+	global_position.y = move_toward(global_position.y, avg_ground_pos + body_height, delta*10)
